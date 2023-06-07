e7d29f28bc0a6e604832b016fb5279d5eab6056a
HridoyHoque <hhridoy155@gmail.com>
2023-06-08 02:40:20 +0600

    implemented post and delete operations for menuCollection

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const verifyJWT = (req, res, next) => {
   const token = authorization.split(' ')[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if(err){
-      return res.status(401).send({ error: true, message: 'unauthorized access' }) 
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
     }
     req.decoded = decoded;
     next();
@@ -56,18 +56,18 @@ async function run() {
       res.send({ token })
     })
 
-    const verifyAdmin = async(req, res, next) => {
+    const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = { email: email};
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
-      if(user?.role !== 'admin'){
-        return res.status(403).send({error: true, message: 'forbidden message'});
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
       }
       next();
     }
     // users related apis
     // get all users
-    app.get('/users',verifyJWT, verifyAdmin, async (req, res) => {
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
@@ -82,17 +82,17 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result)
     })
-   app.get('/users/admin/:email', verifyJWT, async(req, res) => {
-    const email = req.params.email;
-    if(req.decoded.email !== email) {
-      res.send({admin: false})
-    }
-    const query = {email: email}
-    const user = await usersCollection.findOne(query); 
-    const result = {admin: user?.role === 'admin'}
-    res.send(result);
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
 
-   })
+    })
 
     // make user as admin
     app.patch('/users/admin/:id', async (req, res) => {
@@ -118,9 +118,16 @@ async function run() {
       res.send(result);
     });
 
-    app.post('/menu',verifyJWT, verifyAdmin, async (req, res) => {
+    app.post('/menu', verifyJWT, verifyAdmin, async (req, res) => {
       const newItem = req.body;
-      const result = await menuCollection.insertOne();
+      const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    })
+
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
       res.send(result);
     })
 
@@ -136,13 +143,13 @@ async function run() {
       if (!email) {
         res.send([]);
       }
-     const decodedEmail = req.decoded.email;
-     if(email !== decodedEmail) {
-      return res.send(401).send({error: true, message: 'forbidden access'})
-     }
-        const query = { email: email };
-        const result = await cartCollection.find(query).toArray();
-        res.send(result);
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.send(401).send({ error: true, message: 'forbidden access' })
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
     });
 
     app.post('/carts', async (req, res) => {
